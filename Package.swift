54eeb524eedce08698cf6cbe2d71300415e78b16
lazerfocused <tekvyy@gmail.com>
2025-06-19 11:44:04 -0400

    Updated xcframework and updated wrapper project.

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-http-types.git", exact: "1.4.0"),
         .package(url: "https://github.com/apple/swift-openapi-runtime.git", exact: "1.8.2"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.8.4"),
     ],
     targets: [
         .binaryTarget(
@@ -29,6 +30,7 @@ let package = Package(
                 "DynamicSwiftSDK",
                 .product(name: "HTTPTypes", package: "swift-http-types"),
                 .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
             ]
         ),
     ]
